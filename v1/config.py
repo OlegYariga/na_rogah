d04a12d4ee327fafeae8a103b0e89a9a3cffb66a
YarigaOleg <Oyariga@gmail.com>
2019-02-21 13:52:50 +0300

    Change db credentals

--- v1/config.py
+++ v1/config.py
@@ -1,6 +1,6 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:admin@localhost/narogah'
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://hiwtapmbtiopzw:24f8a0de2eae7cdb8964d1d595be39dedd6b4145a0c8519408cf5cbc7f7ae08d@ec2-79-125-6-250.eu-west-1.compute.amazonaws.com/d3l1ljosug4cte'
     SESSION_TYPE = 'filesystem'
     SECRET_KEY = 'somethimngverysercret'
