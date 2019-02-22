c272e654f6d5c4eb1ed95f130a730e38570fa7ee
YarigaOleg <Oyariga@gmail.com>
2019-02-22 20:37:39 +0300

    Fixing paths

--- v1/config.py
+++ v1/config.py
@@ -4,4 +4,4 @@ class Configuration(object):
     SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://hiwtapmbtiopzw:24f8a0de2eae7cdb8964d1d595be39dedd6b4145a0c8519408cf5cbc7f7ae08d@ec2-79-125-6-250.eu-west-1.compute.amazonaws.com/d3l1ljosug4cte'
     SESSION_TYPE = 'filesystem'
     SECRET_KEY = 'somethimngverysercret'
-    UPLOAD_FOLDER = 'https://na-rogah-api.herokuapp.com/photos'
+    UPLOAD_FOLDER = 'https://na-rogah-api.herokuapp.com/v1/photos'
