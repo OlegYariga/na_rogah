56d26d08fc2eb9c07f533a55aad1a6ca62c9fec8
YarigaOleg <Oyariga@gmail.com>
2019-02-23 16:06:23 +0300

    Changed dredentals db

--- v1/config.py
+++ v1/config.py
@@ -3,7 +3,7 @@ class Configuration(object):
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:admin@localhost/narogah'
-    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://hiwtapmbtiopzw:24f8a0de2eae7cdb8964d1d595be39dedd6b4145a0c8519408cf5cbc7f7ae08d@ec2-79-125-6-250.eu-west-1.compute.amazonaws.com/d3l1ljosug4cte'
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://lselsjnaieikfj:d63b9e42bbd21b61816dbf3177141e3773604275ee673617a3d9cf8fbf8a6859@ec2-46-137-170-51.eu-west-1.compute.amazonaws.com/d95c9jl6vpo64i'
     SESSION_TYPE = 'filesystem'
     SECRET_KEY = 'somethimngverysercret'
     UPLOAD_FOLDER = 'C:\\Users\\Doc\\Desktop\\Zappa\\Na_Rogah\\app\\v1\\photos'
