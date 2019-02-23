00bf0c12db87eda76d5efe6c50bc9a8d9f6f1deb
YarigaOleg <Oyariga@gmail.com>
2019-02-23 15:57:41 +0300

    Create db anew

--- v1/migrations/versions/8edcd3e15044_.py
+++ v1/migrations/versions/8edcd3e15044_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 82dc0fd4f33d
+Revision ID: 8edcd3e15044
 Revises: 
-Create Date: 2019-02-23 15:12:05.916444
+Create Date: 2019-02-23 15:54:41.502798
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '82dc0fd4f33d'
+revision = '8edcd3e15044'
 down_revision = None
 branch_labels = None
 depends_on = None
