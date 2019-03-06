622f21645e251d6bb211f2cb6be51df13d7b0267
YarigaOleg <Oyariga@gmail.com>
2019-03-06 16:11:42 +0300

    Added README.txt

--- v1/config.py
+++ v1/config.py
@@ -1,14 +1,14 @@
 class Configuration(object):
-    # DEBUG = True
-    DEBUG = False
+    DEBUG = True
+    # DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:admin@localhost/narogah'
-    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://yhrxhbrfbogimk:47626d88c9e87f23a71d76448009d0cabb6a2921bab9d2db261027c4d18f8536@ec2-46-137-158-249.eu-west-1.compute.amazonaws.com/df14qckbnogd15'
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:admin@localhost/narogah'
+    # SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://yhrxhbrfbogimk:47626d88c9e87f23a71d76448009d0cabb6a2921bab9d2db261027c4d18f8536@ec2-46-137-158-249.eu-west-1.compute.amazonaws.com/df14qckbnogd15'
     SECRET_KEY = 'somethimngverysercrettext'
     SESSION_TYPE = 'filesystem'
     UPLOAD_FOLDER = 'C:\\Users\\Doc\\Desktop\\Zappa\\Na_Rogah\\app\\v1\\photos'
-    # PATH = 'localhost:5000/photos/'
-    PATH = 'https://na-rogah-api.herokuapp.com/photos/'
+    PATH = 'localhost:5000/photos/'
+    # PATH = 'https://na-rogah-api.herokuapp.com/photos/'
 
     SECURITY_PASSWORD_SALT = 'sequritipasswordsalttonotbeenencrypted852753951'
     SECURITY_PASSWORD_HASH = 'plaintext'
