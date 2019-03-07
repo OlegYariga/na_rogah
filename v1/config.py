e0f238ab0a5e284ae13cd9df489e2468f73dc644
YarigaOleg <Oyariga@gmail.com>
2019-03-07 19:10:30 +0300

    Changed database_uri

--- v1/config.py
+++ v1/config.py
@@ -5,9 +5,9 @@ class Configuration(object):
     DEBUG = True
     # DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    # SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     # SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:admin@localhost/narogah'
-    # SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://yhrxhbrfbogimk:47626d88c9e87f23a71d76448009d0cabb6a2921bab9d2db261027c4d18f8536@ec2-46-137-158-249.eu-west-1.compute.amazonaws.com/df14qckbnogd15'
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://yhrxhbrfbogimk:47626d88c9e87f23a71d76448009d0cabb6a2921bab9d2db261027c4d18f8536@ec2-46-137-158-249.eu-west-1.compute.amazonaws.com/df14qckbnogd15'
     SECRET_KEY = 'somethimngverysercrettext'
     SESSION_TYPE = 'filesystem'
     UPLOAD_FOLDER = 'C:\\Users\\Doc\\Desktop\\Zappa\\Na_Rogah\\app\\v1\\photos'
