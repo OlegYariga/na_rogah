16e7348f67246e1e77cbe18df20bbb9661188f19
YarigaOleg <Oyariga@gmail.com>
2019-03-23 21:08:04 +0300

    asdasdasd

--- view.py
+++ view.py
@@ -259,6 +259,10 @@ def reg_user():
         # Get data and convert into JSON (email, password, code
         data = request.data
         json_data = json.loads(data)
+        print("\n \n \n \n \n ")
+        print('email from json:::::' + str(json_data['email']))
+        print('code in session::::' + str(session['rayajowah@mailfavorite.com']))
+        print("\n \n \n \n \n ")
         if str(json_data['email']) in session:
             # If code from email is correct
             if str(json_data['code']) == str(session[json_data['email']]):
